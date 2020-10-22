5360a71723e4cf90a33f0a80b0c533e6679365bc
SarthakSri98 <sarthaksri1998@gmail.com>
2020-10-22 11:12:59 +0530

    feat(): use for loop to generate box

--- src/components/box/box.js
+++ src/components/box/box.js
@@ -11,24 +11,16 @@ class Box extends React.Component{
     {
         return (
             <div>
-            <div class="box">
-                <div class="row1">
-                    <button id='1'>-</button>
-                    <button id='2'>-</button>
-                    <button id='3'>-</button>
+                <div className="box">
+                {
+                    this.props.matrix.map((row, rowIndex) => {
+                        return <div>{row.map((square, colIndex) =>{
+                        return <button>{this.props.matrix[rowIndex][colIndex]}</button>
+                        })}</div>
+                    })
+                }
                 </div>
-                <div class="row2">
-                    <button id='4'>-</button>
-                    <button id='5'>-</button>
-                    <button id='6'>-</button>
-                </div>
-                <div class="row1">
-                    <button id='7'>-</button>
-                    <button id='8'>-</button>
-                    <button id='9'>-</button>
-                </div>
-            </div>
-            <button id="reset">Reset</button>
+                <button id="reset">Reset</button>
             </div>
         );
     }
